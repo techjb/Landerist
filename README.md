258e69758de7fb14eec38399163c9f27fa15c8ba
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2023-04-14 09:38:10 +0200

    readme.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landerist
+
+Landerist is an open project for the decentralization of real estate listings.
+
+## Installation
+
+Download the project and open it in Visual Studio. In the file /landerist_library/Configuration/Config.cs, set the value of the PrivateConfig variables.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ landerist_library/Configuration/Config.cs
@@ -0,0 +1,76 @@
+﻿namespace landerist_library.Configuration
+{
+    public class Config
+    {
+        private static bool ConfigurationProduction = true;
+
+        private static readonly string VERSION_PRODUCCION = "1.00";
+
+        private static readonly string VERSION_LOCAL = new Random().Next(1000, 9999).ToString();
+        public static string? VERSION { get; set; }
+
+        public const string DATABASE_NAME = PrivateConfig.DATABASE_NAME;
+
+        public const string DATABASE_USER = PrivateConfig.DATABASE_USER;
+
+        public const string DATABASE_PW = PrivateConfig.DATABASE_PW;        
+
+        private static readonly string CONNECTION_USER =
+            "User ID=" + DATABASE_USER + ";" +
+            "Password=" + DATABASE_PW + ";" +
+            "Connect Timeout=100000;";
+
+        private static readonly string DATASOURCE_LOCAL = PrivateConfig.DATASOURCE_LOCAL;
+
+        private static readonly string DATASOURCE_PRODUCTION = PrivateConfig.DATASOURCE_PRODUCTION;
+
+        public static string? DATASOURCE { get; set; }
+
+        private static readonly string DATABASE_CONNECTION_LOCAL = CONNECTION_USER +
+            "Data Source=" + DATASOURCE_LOCAL + ";" +
+            "Initial Catalog=";
+
+        private static readonly string DATABASE_CONNECTION_PRODUCTION = CONNECTION_USER +
+            "Data Source=" + DATASOURCE_PRODUCTION + ";" +
+            "Initial Catalog=";
+
+        public static string? DATABASE_CONNECTION { get; set; }
+
+        public static readonly string OPENAI_API_KEY = PrivateConfig.OPENAI_API_KEY;
+
+        public static string? EXPORT_DIRECTORY { get; set; }
+
+        private static readonly string EXPORT_DIRECTORY_LOCAL = PrivateConfig.EXPORT_DIRECTORY_LOCAL;
+
+        private static readonly string EXPORT_DIRECTORY_PRODUCTION = PrivateConfig.EXPORT_DIRECTORY_PRODUCTION;
+
+        public static string USER_AGENT = "Mozilla/5.0 (compatible; AcmeInc/1.0)";
+
+
+        public static bool IsConfigurationProduction()
+        {
+            return ConfigurationProduction;
+        }
+
+        public static void Init(bool configurationProduction)
+        {
+            ConfigurationProduction = configurationProduction;
+
+            VERSION = ConfigurationProduction ?
+                VERSION_PRODUCCION :
+                VERSION_LOCAL;
+
+            DATASOURCE = ConfigurationProduction ?
+                DATASOURCE_PRODUCTION :
+                DATASOURCE_LOCAL;
+
+            DATABASE_CONNECTION = ConfigurationProduction ?
+                DATABASE_CONNECTION_PRODUCTION :
+                DATABASE_CONNECTION_LOCAL;
+
+            EXPORT_DIRECTORY = configurationProduction ?
+                EXPORT_DIRECTORY_LOCAL :
+                EXPORT_DIRECTORY_PRODUCTION;
+        }
+    }
+}
