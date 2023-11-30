c58a4cc49969cfc1801a4991daee389ab1a63035
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2023-11-30 07:27:33 +0100

    updates

--- landerist_library/Parse/Listing/ChatGPT/ParseListingResponse.cs
+++ landerist_library/Parse/Listing/ChatGPT/ParseListingResponse.cs
@@ -1,9 +1,7 @@
-﻿using landerist_library.Websites;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using landerist_library.Tools;
+using landerist_library.Websites;
+using landerist_orels.ES;
+using System.Text.Json.Nodes;
 
 namespace landerist_library.Parse.Listing.ChatGPT
 {
@@ -76,11 +74,451 @@ namespace landerist_library.Parse.Listing.ChatGPT
         public bool? PermiteMascotas { get; set; } = null;
 
         public bool? TieneSistemasDeSeguridad { get; set; } = null;
-        
+
+
+        protected const string OPERACION_VENTA = "venta";
+
+        protected const string OPERACION_ALQUILER = "alquiler";
+
+        protected const string TIPO_DE_INMUEBLE_VIVIENDA = "vivienda";
+
+        protected const string TIPO_DE_INMUEBLE_DORMITORIO = "dormitorio";
+
+        protected const string TIPO_DE_INMUEBLE_LOCAL_COMERCIAL = "local_comercial";
+
+        protected const string TIPO_DE_INMUEBLE_NAVE_INDUSTRIAL = "nave_industrial";
+
+        protected const string TIPO_DE_INMUEBLE_GARAJE = "garaje";
+
+        protected const string TIPO_DE_INMUEBLE_TRASTERO = "trastero";
+
+        protected const string TIPO_DE_INMUEBLE_OFICINA = "oficina";
+
+        protected const string TIPO_DE_INMUEBLE_PARCELA = "terreno_o_parcela";
+
+        protected const string TIPO_DE_INMUEBLE_EDIFICIO = "edificio";
+
+        protected const string SUBTIPO_DE_INMUEBLE_PISO = "piso";
+
+        protected const string SUBTIPO_DE_INMUEBLE_APARTAMENTO = "apartamento";
+
+        protected const string SUBTIPO_DE_INMUEBLE_ÁTICO = "ático";
+
+        protected const string SUBTIPO_DE_INMUEBLE_BUNGALOW = "bungalow";
+
+        protected const string SUBTIPO_DE_INMUEBLE_DUPLEX = "dúplex";
+
+        protected const string SUBTIPO_DE_INMUEBLE_CHALET_INDEPENDIENTE = "chalet_independiente";
+
+        protected const string SUBTIPO_DE_INMUEBLE_CHALET_PAREADO = "chalet_pareado";
+
+        protected const string SUBTIPO_DE_INMUEBLE_CHALET_ADOSADO = "chalet_adosado";
+
+        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_URBANA = "parcela_urbana";
+
+        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_URBANIZABLE = "parcela_urbanizable";
+
+        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_NO_URBANIZABLE = "parcela_no_urbanizable";
+
+        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_OBRA_NUEVA = "obra_nueva";
+
+        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_BUENO = "buen_estado";
+
+        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_A_REFORMAR = "a_reformar";
+
+        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_EN_RUINAS = "en_ruinas";
+
+
+        protected static readonly JsonArray TiposDeOperación = new()
+        {
+            OPERACION_VENTA,
+            OPERACION_ALQUILER
+        };
+
+        protected static readonly JsonArray TiposDeInmueble = new()
+        {
+            TIPO_DE_INMUEBLE_VIVIENDA,
+            TIPO_DE_INMUEBLE_DORMITORIO,
+            TIPO_DE_INMUEBLE_LOCAL_COMERCIAL,
+            TIPO_DE_INMUEBLE_NAVE_INDUSTRIAL,
+            TIPO_DE_INMUEBLE_GARAJE,
+            TIPO_DE_INMUEBLE_TRASTERO,
+            TIPO_DE_INMUEBLE_OFICINA,
+            TIPO_DE_INMUEBLE_PARCELA,
+            TIPO_DE_INMUEBLE_EDIFICIO
+        };
+
+        protected static readonly JsonArray SubtiposDeInmueble = new()
+        {
+            SUBTIPO_DE_INMUEBLE_PISO,
+            SUBTIPO_DE_INMUEBLE_APARTAMENTO,
+            SUBTIPO_DE_INMUEBLE_ÁTICO,
+            SUBTIPO_DE_INMUEBLE_BUNGALOW,
+            SUBTIPO_DE_INMUEBLE_DUPLEX,
+            SUBTIPO_DE_INMUEBLE_CHALET_INDEPENDIENTE,
+            SUBTIPO_DE_INMUEBLE_CHALET_PAREADO,
+            SUBTIPO_DE_INMUEBLE_CHALET_ADOSADO,
+            SUBTIPO_DE_INMUEBLE_PARCELA_URBANA,
+            SUBTIPO_DE_INMUEBLE_PARCELA_URBANIZABLE,
+            SUBTIPO_DE_INMUEBLE_PARCELA_NO_URBANIZABLE
+        };
+
+        protected static readonly JsonArray EstadosDeLaConstrucción = new()
+        {
+            ESTADO_DE_LA_CONSTRUCCIÓN_OBRA_NUEVA,
+            ESTADO_DE_LA_CONSTRUCCIÓN_BUENO,
+            ESTADO_DE_LA_CONSTRUCCIÓN_A_REFORMAR,
+            ESTADO_DE_LA_CONSTRUCCIÓN_EN_RUINAS
+        };
 
         public landerist_orels.ES.Listing? ToListing(Page page)
         {
+            if (!IsValidResponse())
+            {
+                return null;
+            }
+
+            var listing = new landerist_orels.ES.Listing
+            {
+                guid = page.UriHash,
+                listingStatus = GetListingStatus(),
+                listingDate = GetListingDate(),
+                operation = GetOperation(),
+                propertyType = GetPropertyType(),
+                propertySubtype = GetPropertySubtype(),
+                price = GetPropertyPrice(),
+                description = GetDescription(),
+                dataSourceName = GetDataSourceName(page),
+                dataSourceGuid = GetDataSourceGuid(),
+                dataSourceUpdate = GetDataSourceUpdate(),
+                dataSourceUrl = GetDataSourceUrl(page),
+                contactPhone = GetPhone(),
+                contactEmail = GetEmail(),
+                address = GetAddress(),
+                cadastralReference = GetCadastralReference(),
+                propertySize = TamañoDelInmueble,
+                landSize = TamañoDeLaParcela,
+                constructionYear = GetConstrunctionYear(),
+                constructionStatus = GetConstructionStatus(),
+                floors = GetFloors(),
+                floor = GetFloor(),
+                bedrooms = GetBedrooms(),
+                bathrooms = GetBathrooms(),
+                parkings = GetParkings(),
+                terrace = TieneTerraza,
+                garden = TieneJardín,
+                garage = TieneGaraje,
+                motorbikeGarage = TieneParkingParaMoto,
+                pool = TienePiscina,
+                lift = TieneAscensor,
+                disabledAccess = TieneAccesoParaDiscapacitados,
+                storageRoom = TieneTrastero,
+                furnished = EstáAmueblado,
+                nonFurnished = NoEstáAmueblado,
+                heating = TienCalefácción,
+                airConditioning = TienAireAcondicionado,
+                petsAllowed = PermiteMascotas,
+                securitySystems = TieneSistemasDeSeguridad,
+            };
+            return listing;
+        }
+
+        private bool IsValidResponse()
+        {
+            return
+                IsValidTipoDeOperacion() &&
+                IsValidTipoDeInmueble();
+        }
+
+        private bool IsValidTipoDeOperacion()
+        {
+            if (TipoDeOperación != null)
+            {
+                return TiposDeOperación.Contains(TipoDeOperación);
+            }
+            return false;
+        }
+
+        private bool IsValidTipoDeInmueble()
+        {
+            if (TipoDeInmueble != null)
+            {
+                return TipoDeInmueble.Contains(TipoDeInmueble);
+            }
+            return false;
+        }
+
+        private static ListingStatus GetListingStatus()
+        {
+            return ListingStatus.published;
+        }
+
+        private DateTime GetListingDate()
+        {
+            if (FechaDePublicación != null)
+            {
+                if (DateTime.TryParse(FechaDePublicación, out DateTime listingDate))
+                {
+                    if (listingDate < DateTime.Now)
+                    {
+                        return listingDate;
+                    }
+                }
+            }
+            return DateTime.Now;
+        }
+
+        private Operation GetOperation()
+        {
+            return TipoDeOperación switch
+            {
+                OPERACION_VENTA => Operation.sell,
+                OPERACION_ALQUILER => Operation.rent,
+                _ => Operation.sell,
+            };
+        }
+
+        private PropertyType GetPropertyType()
+        {
+            return TipoDeInmueble switch
+            {
+                TIPO_DE_INMUEBLE_VIVIENDA => PropertyType.home,
+                TIPO_DE_INMUEBLE_DORMITORIO => PropertyType.room,
+                TIPO_DE_INMUEBLE_LOCAL_COMERCIAL => PropertyType.premise,
+                TIPO_DE_INMUEBLE_NAVE_INDUSTRIAL => PropertyType.industrial,
+                TIPO_DE_INMUEBLE_GARAJE => PropertyType.garage,
+                TIPO_DE_INMUEBLE_TRASTERO => PropertyType.storage,
+                TIPO_DE_INMUEBLE_OFICINA => PropertyType.office,
+                TIPO_DE_INMUEBLE_PARCELA => PropertyType.land,
+                TIPO_DE_INMUEBLE_EDIFICIO => PropertyType.building,
+                _ => PropertyType.home,
+            };
+        }
+
+        private PropertySubtype? GetPropertySubtype()
+        {
+            if (SubtipoDeInmueble != null)
+            {
+                PropertySubtype? propertySubtype = GetPropertySubtipeValue();
+                if (IsValidPropertySubtype(propertySubtype))
+                {
+                    return propertySubtype;
+                }
+            }
             return null;
+        }
+
+        private PropertySubtype? GetPropertySubtipeValue()
+        {
+            return SubtipoDeInmueble switch
+            {
+                SUBTIPO_DE_INMUEBLE_PISO => (PropertySubtype?)PropertySubtype.flat,
+                SUBTIPO_DE_INMUEBLE_APARTAMENTO => (PropertySubtype?)PropertySubtype.appartment,
+                SUBTIPO_DE_INMUEBLE_ÁTICO => (PropertySubtype?)PropertySubtype.penthouse,
+                SUBTIPO_DE_INMUEBLE_BUNGALOW => (PropertySubtype?)PropertySubtype.bungalow,
+                SUBTIPO_DE_INMUEBLE_DUPLEX => (PropertySubtype?)PropertySubtype.duplex,
+                SUBTIPO_DE_INMUEBLE_CHALET_INDEPENDIENTE => (PropertySubtype?)PropertySubtype.detached,
+                SUBTIPO_DE_INMUEBLE_CHALET_PAREADO => (PropertySubtype?)PropertySubtype.semi_detached,
+                SUBTIPO_DE_INMUEBLE_CHALET_ADOSADO => (PropertySubtype?)PropertySubtype.terraced,
+                SUBTIPO_DE_INMUEBLE_PARCELA_URBANA => (PropertySubtype?)PropertySubtype.developed,
+                SUBTIPO_DE_INMUEBLE_PARCELA_URBANIZABLE => (PropertySubtype?)PropertySubtype.buildable,
+                SUBTIPO_DE_INMUEBLE_PARCELA_NO_URBANIZABLE => (PropertySubtype?)PropertySubtype.non_building,
+                _ => null,
+            };
+        }
+
+        private bool IsValidPropertySubtype(PropertySubtype? propertySubtype)
+        {
+            if (propertySubtype == null)
+            {
+                return true;
+            }
+            PropertySubtype propertySubtypeNotNull = (PropertySubtype)propertySubtype;
+            var propertyType = GetPropertyType();
+            switch (propertyType)
+            {
+                case PropertyType.home:
+                    {
+                        return IsValidSubtypeHome(propertySubtypeNotNull);
+                    }
+                case PropertyType.land:
+                    {
+                        return IsValidSubtypeLand(propertySubtypeNotNull);
+                    }
+                default: break;
+            }
+            return false;
+        }
+
+        private static bool IsValidSubtypeHome(PropertySubtype propertySubtype)
+        {
+            return
+                propertySubtype.Equals(PropertySubtype.flat) ||
+                propertySubtype.Equals(PropertySubtype.appartment) ||
+                propertySubtype.Equals(PropertySubtype.penthouse) ||
+                propertySubtype.Equals(PropertySubtype.bungalow) ||
+                propertySubtype.Equals(PropertySubtype.duplex) ||
+                propertySubtype.Equals(PropertySubtype.detached) ||
+                propertySubtype.Equals(PropertySubtype.semi_detached) ||
+                propertySubtype.Equals(PropertySubtype.terraced);
+        }
+
+        private static bool IsValidSubtypeLand(PropertySubtype propertySubtype)
+        {
+            return
+                propertySubtype.Equals(PropertySubtype.developed) ||
+                propertySubtype.Equals(PropertySubtype.buildable) ||
+                propertySubtype.Equals(PropertySubtype.non_building);
+        }
+
+        private Price? GetPropertyPrice()
+        {
+            if (PrecioDelAnuncio == null)
+            {
+                return null;
+            }
+
+            var price = (decimal)PrecioDelAnuncio;
+            if (price <= 0)
+            {
+                return null;
+            }
+            return new Price(price, Currency.EUR);
+        }
+
+        private string? GetDescription()
+        {
+            if (string.IsNullOrEmpty(DescripciónDelAnuncio))
+            {
+                return null;
+            }
+            return Strings.Clean(DescripciónDelAnuncio);
+        }
+
+        private static string GetDataSourceName(Page page)
+        {
+            return page.Website.Host;
+        }
+
+        private string? GetDataSourceGuid()
+        {
+            if (string.IsNullOrEmpty(ReferenciaDelAnuncio))
+            {
+                return null;
+            }
+            return Strings.Clean(ReferenciaDelAnuncio);
+        }
+
+        private static DateTime GetDataSourceUpdate()
+        {
+            return DateTime.Now;
+        }
+
+        private static Uri GetDataSourceUrl(Page page)
+        {
+            return page.Uri;
+        }
+
+        private string? GetPhone()
+        {
+            if (string.IsNullOrEmpty(TeléfonoDeContacto))
+            {
+                return null;
+            }
+            TeléfonoDeContacto = Strings.Clean(TeléfonoDeContacto);
+            if (!Validate.Phone(TeléfonoDeContacto))
+            {
+                return null;
+            }
+            return TeléfonoDeContacto;
+        }
+
+        private string? GetEmail()
+        {
+            if (string.IsNullOrEmpty(EmailDeContacto))
+            {
+                return null;
+            }
+            EmailDeContacto = Strings.Clean(EmailDeContacto);
+            EmailDeContacto = Strings.RemoveSpaces(EmailDeContacto);
+            if (!Validate.Email(EmailDeContacto))
+            {
+                return null;
+            }
+            return EmailDeContacto;
+        }
+
+        private string? GetAddress()
+        {
+            if (string.IsNullOrEmpty(DirecciónDelInmueble))
+            {
+                return null;
+            }
+            return Strings.Clean(DirecciónDelInmueble);
+        }
+
+        private string? GetCadastralReference()
+        {
+            if (string.IsNullOrEmpty(ReferenciaCatastral))
+            {
+                return null;
+            }
+            ReferenciaCatastral = Strings.Clean(ReferenciaCatastral);
+            if (!Validate.CadastralReference(ReferenciaCatastral))
+            {
+                return null;
+            }
+            return ReferenciaCatastral;
+        }
+
+        private int? GetConstrunctionYear()
+        {
+            return (int?)AñoDeConstrucción;
+        }
+
+        private ConstructionStatus? GetConstructionStatus()
+        {
+            if (string.IsNullOrEmpty(EstadoDeLaConstrucción))
+            {
+                return null;
+            }
+            switch (EstadoDeLaConstrucción)
+            {
+                case ESTADO_DE_LA_CONSTRUCCIÓN_OBRA_NUEVA: return ConstructionStatus.@new;
+                case ESTADO_DE_LA_CONSTRUCCIÓN_BUENO: return ConstructionStatus.good;
+                case ESTADO_DE_LA_CONSTRUCCIÓN_A_REFORMAR: return ConstructionStatus.for_renovation;
+                case ESTADO_DE_LA_CONSTRUCCIÓN_EN_RUINAS: return ConstructionStatus.refurbished;
+                default: return null;
+            }
+        }
+
+        private int? GetFloors()
+        {
+            return (int?)PlantasDelEdificio;
+        }
+
+        private string? GetFloor()
+        {
+            if (string.IsNullOrEmpty(PlantaDelInmueble))
+            {
+                return null;
+            }
+            return Strings.Clean(PlantaDelInmueble);
+        }
+
+        private int? GetBedrooms()
+        {
+            return (int?)NúmeroDeDormitorios;
+        }
+
+        private int? GetBathrooms()
+        {
+            return (int?)NúmeroDeBaños;
+        }
+
+        private int? GetParkings()
+        {
+            return (int?)NúmeroDeParkings;
         }
     }
 }
--- landerist_library/Parse/Listing/ChatGPT/ParseListingTool.cs
+++ landerist_library/Parse/Listing/ChatGPT/ParseListingTool.cs
@@ -5,106 +5,13 @@ namespace landerist_library.Parse.Listing.ChatGPT
 {
     public class ParseListingTool : ParseListingResponse
     {
-        protected const string OPERACION_VENTA = "venta";
-
-        protected const string OPERACION_ALQUILER = "alquiler";
-
-        protected const string TIPO_DE_INMUEBLE_VIVIENDA = "vivienda";
-
-        protected const string TIPO_DE_INMUEBLE_DORMITORIO = "dormitorio";
-
-        protected const string TIPO_DE_INMUEBLE_LOCAL_COMERCIAL = "local_comercial";
-
-        protected const string TIPO_DE_INMUEBLE_NAVE_INDUSTRIAL = "nave_industrial";
-
-        protected const string TIPO_DE_INMUEBLE_GARAJE = "garaje";
-
-        protected const string TIPO_DE_INMUEBLE_TRASTERO = "trastero";
-
-        protected const string TIPO_DE_INMUEBLE_OFICINA = "oficina";
-
-        protected const string TIPO_DE_INMUEBLE_PARCELA = "terreno_o_parcela";
-
-        protected const string TIPO_DE_INMUEBLE_EDIFICIO = "edificio";
-
-        protected const string SUBTIPO_DE_INMUEBLE_PISO = "piso";
-
-        protected const string SUBTIPO_DE_INMUEBLE_APARTAMENTO = "apartamento";
-
-        protected const string SUBTIPO_DE_INMUEBLE_ÁTICO = "ático";
-
-        protected const string SUBTIPO_DE_INMUEBLE_BUNGALOW = "bungalow";
-
-        protected const string SUBTIPO_DE_INMUEBLE_DUPLEX = "dúplex";
-
-        protected const string SUBTIPO_DE_INMUEBLE_CHALET_INDEPENDIENTE = "chalet_independiente";
-
-        protected const string SUBTIPO_DE_INMUEBLE_CHALET_PAREADO = "chalet_pareado";
-
-        protected const string SUBTIPO_DE_INMUEBLE_CHALET_ADOSADO = "chalet_adosado";
-
-        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_URBANA = "parcela_urbana";
-
-        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_URBANIZABLE = "parcela_urbanizable";
-
-        protected const string SUBTIPO_DE_INMUEBLE_PARCELA_NO_URBANIZABLE = "parcela_no_urbanizable";
-
-        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_OBRA_NUEVA = "obra_nueva";
-
-        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_BUENO = "buen_estado";
-
-        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_A_REFORMAR = "a_reformar";
-
-        protected const string ESTADO_DE_LA_CONSTRUCCIÓN_EN_RUINAS = "en_ruinas";
-
         private static readonly string FunctionName = "data_extractor";
         private static readonly string FunctionDescription = "Extract the data from the advertisement.";
-
-        private static readonly JsonArray TiposDeOperación = new()
-        {
-            OPERACION_VENTA,
-            OPERACION_ALQUILER
-        };
-
-        private static readonly JsonArray TiposDeInmueble = new()
-        {
-            TIPO_DE_INMUEBLE_VIVIENDA,
-            TIPO_DE_INMUEBLE_DORMITORIO,
-            TIPO_DE_INMUEBLE_LOCAL_COMERCIAL,
-            TIPO_DE_INMUEBLE_NAVE_INDUSTRIAL,
-            TIPO_DE_INMUEBLE_GARAJE,
-            TIPO_DE_INMUEBLE_TRASTERO,
-            TIPO_DE_INMUEBLE_OFICINA,
-            TIPO_DE_INMUEBLE_PARCELA,
-            TIPO_DE_INMUEBLE_EDIFICIO
-        };
-
-        private static readonly JsonArray SubtiposDeInmueble = new()
-        {
-            SUBTIPO_DE_INMUEBLE_PISO,
-            SUBTIPO_DE_INMUEBLE_APARTAMENTO,
-            SUBTIPO_DE_INMUEBLE_ÁTICO,
-            SUBTIPO_DE_INMUEBLE_BUNGALOW,
-            SUBTIPO_DE_INMUEBLE_DUPLEX,
-            SUBTIPO_DE_INMUEBLE_CHALET_INDEPENDIENTE,
-            SUBTIPO_DE_INMUEBLE_CHALET_PAREADO,
-            SUBTIPO_DE_INMUEBLE_CHALET_ADOSADO,
-            SUBTIPO_DE_INMUEBLE_PARCELA_URBANA,
-            SUBTIPO_DE_INMUEBLE_PARCELA_URBANIZABLE,
-            SUBTIPO_DE_INMUEBLE_PARCELA_NO_URBANIZABLE
-        };
-
-        private static readonly JsonArray EstadosDeLaConstrucción = new()
-        {
-            ESTADO_DE_LA_CONSTRUCCIÓN_OBRA_NUEVA,
-            ESTADO_DE_LA_CONSTRUCCIÓN_BUENO,
-            ESTADO_DE_LA_CONSTRUCCIÓN_A_REFORMAR,
-            ESTADO_DE_LA_CONSTRUCCIÓN_EN_RUINAS
-        };
 
         public static Function GetTool()
         {
             var properties = new JsonObject();
+
             AddString(properties, nameof(FechaDePublicación), "fecha de publicación");
             AddEnum(properties, nameof(TipoDeOperación), "tipo de operación", TiposDeOperación);
             AddEnum(properties, nameof(TipoDeInmueble), "tipo de inmueble", TiposDeInmueble);
@@ -117,6 +24,7 @@ namespace landerist_library.Parse.Listing.ChatGPT
             AddString(properties, nameof(DirecciónDelInmueble), "dirección del inmueble");
             AddString(properties, nameof(ReferenciaCatastral), "referencia catastral");
             AddNumber(properties, nameof(TamañoDelInmueble), "metros cuadrados del inmueble");
+            AddNumber(properties, nameof(TamañoDeLaParcela), "metros cuadrados de la parcela");
             AddNumber(properties, nameof(AñoDeConstrucción), "año de construcción");
             AddEnum(properties, nameof(EstadoDeLaConstrucción), "estado de la construcción", EstadosDeLaConstrucción);
             AddNumber(properties, nameof(PlantasDelEdificio), "plantas del edificio");
@@ -151,41 +59,34 @@ namespace landerist_library.Parse.Listing.ChatGPT
 
         private static void AddString(JsonObject jsonObject, string name, string description)
         {
-            var property = new JsonObject
-            {
-                ["type"] = "string",
-                ["description"] = description
-            };
-            jsonObject.Add(name, property);
+            Add(jsonObject, name, "string", description);            
         }
 
         private static void AddEnum(JsonObject jsonObject, string name, string description, JsonArray jsonArray)
         {
-            var property = new JsonObject
-            {
-                ["type"] = "string",
-                ["description"] = description,
-                ["enum"] = jsonArray
-            };
-            jsonObject.Add(name, property);
+            Add(jsonObject, name, "string", description, jsonArray);
         }
 
         private static void AddNumber(JsonObject jsonObject, string name, string description)
         {
-            var property = new JsonObject
-            {
-                ["type"] = "number",
-                ["description"] = description
-            };
-            jsonObject.Add(name, property);
+            Add(jsonObject, name, "number", description);
         }
         private static void AddBoolean(JsonObject jsonObject, string name, string description)
         {
+            Add(jsonObject, name, "boolean", description);
+        }
+
+        private static void Add(JsonObject jsonObject, string name, string type, string description, JsonArray? jsonArray = null
+        {
             var property = new JsonObject
             {
-                ["type"] = "boolean",
+                ["type"] = type,
                 ["description"] = description
             };
+            if (jsonArray != null)
+            {
+                property.Add("enum", jsonArray);
+            }
             jsonObject.Add(name, property);
         }
     }
