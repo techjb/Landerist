4e8f717102b7990a6c5507720dcfecc835181fba
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2024-04-24 10:17:02 +0200

    updates

--- landerist_library/Configuration/Config.cs
+++ landerist_library/Configuration/Config.cs
@@ -4,7 +4,7 @@
     {
         private static bool ConfigurationProduction = true;
 
-        public static readonly string VERSION = "1.85";
+        public static readonly string VERSION = "1.86";
 
         public static readonly bool SET_LATLNG_LAUID_AND_MEDIA_TO_LISTING = true;
 
