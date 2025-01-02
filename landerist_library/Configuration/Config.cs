a0f9dc722214a38226c728adee8361eb47fa9786
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2025-01-02 12:08:24 +0100

    updtes

--- landerist_library/Configuration/Config.cs
+++ landerist_library/Configuration/Config.cs
@@ -7,7 +7,7 @@ namespace landerist_library.Configuration
     {
         private static bool ConfigurationProduction = true;
 
-        public static readonly string VERSION = "3.78";
+        public static readonly string VERSION = "3.80";
 
         public static readonly bool INDEXER_ENABLED = true;
 
