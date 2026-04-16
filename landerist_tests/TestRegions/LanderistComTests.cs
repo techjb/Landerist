8b903b9e693c6b426c352d2a30baf0e1248de1c0
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2026-04-16 11:35:05 +0200

    d

--- landerist_tests/TestRegions/LanderistComTests.cs
+++ landerist_tests/TestRegions/LanderistComTests.cs
@@ -21,7 +21,7 @@ namespace landerist_tests
             //landerist_library.Landerist_com.Landerist_com.UpdateDownloadsPage();
             //landerist_library.Landerist_com.Landerist_com.UpdateHostsPage();            
             //landerist_library.Landerist_com.Landerist_com.UpdateHostsPage();
-            landerist_library.Landerist_com.Landerist_com.UpdateAllPages();
+            //landerist_library.Landerist_com.Landerist_com.UpdateAllPages();
 
             //landerist_library.Landerist_com.Landerist_com.InvalidateCloudFront();
         }
