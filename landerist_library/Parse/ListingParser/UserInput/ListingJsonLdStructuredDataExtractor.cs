6d5cdc33fab306d7595adb064999add42e03f955
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2026-05-07 16:02:22 +0200

    json ld

--- landerist_library/Parse/ListingParser/UserInput/ListingJsonLdStructuredDataExtractor.cs
+++ landerist_library/Parse/ListingParser/UserInput/ListingJsonLdStructuredDataExtractor.cs
@@ -6,56 +6,6 @@ namespace landerist_library.Parse.ListingParser.UserInput
 {
     internal static class ListingJsonLdStructuredDataExtractor
     {
-        private static readonly HashSet<string> JsonLdPropertiesToExtract = new(StringComparer.OrdinalIgnoreCase)
-        {
-            "@type",
-            "type",
-            "name",
-            "headline",
-            "title",
-            "description",
-            "url",
-            "mainEntityOfPage",
-            "image",
-            "images",
-            "photo",
-            "photos",
-            "thumbnail",
-            "thumbnailUrl",
-            "contentUrl",
-            "address",
-            "streetAddress",
-            "addressLocality",
-            "addressRegion",
-            "postalCode",
-            "addressCountry",
-            "geo",
-            "latitude",
-            "longitude",
-            "price",
-            "priceCurrency",
-            "lowPrice",
-            "highPrice",
-            "availability",
-            "floorSize",
-            "numberOfRooms",
-            "numberOfBedrooms",
-            "numberOfBathroomsTotal",
-            "rooms",
-            "bedrooms",
-            "bathrooms",
-            "telephone",
-            "email",
-            "seller",
-            "provider",
-            "broker",
-            "brand",
-            "datePosted",
-            "sku",
-            "productID",
-            "identifier",
-        };
-
         public static string? Extract(HtmlDocument htmlDocument)
         {
             var scripts = htmlDocument.DocumentNode.SelectNodes(
@@ -66,7 +16,7 @@ namespace landerist_library.Parse.ListingParser.UserInput
                 return null;
             }
 
-            Dictionary<string, List<string>> values = new(StringComparer.OrdinalIgnoreCase);
+            List<string> blocks = [];
             foreach (var script in scripts)
             {
                 string json = HttpUtility.HtmlDecode(script.InnerText)?.Trim() ?? string.Empty;
@@ -77,85 +27,17 @@ namespace landerist_library.Parse.ListingParser.UserInput
 
                 try
                 {
-                    CollectStructuredData(JToken.Parse(json), values);
+                    JToken.Parse(json);
+                    blocks.Add("JSON-LD: " + json);
                 }
                 catch
                 {
                 }
             }
 
-            return ListingStructuredDataValues.FormatBlock("JSON-LD", values);
-        }
-
-        private static void CollectStructuredData(JToken token, Dictionary<string, List<string>> values)
-        {
-            if (token is JObject jsonObject)
-            {
-                foreach (var property in jsonObject.Properties())
-                {
-                    string propertyName = property.Name.TrimStart('@');
-                    if (JsonLdPropertiesToExtract.Contains(property.Name) || JsonLdPropertiesToExtract.Contains(propertyName))
-                    {
-                        foreach (string value in FlattenStructuredValues(property.Value).Take(12))
-                        {
-                            ListingStructuredDataValues.Add(values, propertyName, value);
-                        }
-                    }
-
-                    CollectStructuredData(property.Value, values);
-                }
-
-                return;
-            }
-
-            if (token is JArray jsonArray)
-            {
-                foreach (var item in jsonArray)
-                {
-                    CollectStructuredData(item, values);
-                }
-            }
-        }
-
-        private static IEnumerable<string> FlattenStructuredValues(JToken token)
-        {
-            if (token is JValue jsonValue)
-            {
-                string? text = jsonValue.Value?.ToString();
-                if (!string.IsNullOrWhiteSpace(text))
-                {
-                    yield return text;
-                }
-
-                yield break;
-            }
-
-            if (token is JArray array)
-            {
-                foreach (var item in array)
-                {
-                    foreach (string flattenedValue in FlattenStructuredValues(item))
-                    {
-                        yield return flattenedValue;
-                    }
-                }
-
-                yield break;
-            }
-
-            if (token is JObject jsonObject)
-            {
-                foreach (var property in jsonObject.Properties())
-                {
-                    if (property.Value is JValue)
-                    {
-                        foreach (string flattenedValue in FlattenStructuredValues(property.Value))
-                        {
-                            yield return $"{property.Name}:{flattenedValue}";
-                        }
-                    }
-                }
-            }
+            return blocks.Count == 0
+                ? null
+                : string.Join(Environment.NewLine, blocks.Distinct(StringComparer.Ordinal));
         }
     }
 }
--- landerist_tests/TestRegions/ScrapperTests.cs
+++ landerist_tests/TestRegions/ScrapperTests.cs
@@ -16,7 +16,7 @@ namespace landerist_tests
             //new Scraper().ProcessPages();
             //new Scraper().Scrape(page, false);
 
-            //new landerist_library.Scrape.Scraper().Scrape("https://www.remax.es/buscador-de-inmuebles/venta/atico/alicante-alacant/torrevieja/todos/RE-V-VAL-015/", false);
+            new landerist_library.Scrape.Scraper().Scrape("https://www.engelvoelkers.com/es/es/exposes/4ede1607-6c8d-5975-b660-6ead5645805c", false);
             //new Scraper().TestSinglePage();
             //landerist_library.Scrape.PageSelector.SelectTop1();
             //Console.WriteLine("Block: " + WebsitesThrottle.Block(page.Website));
