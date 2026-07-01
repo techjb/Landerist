76d64abe7a8588612fb9eaca0f2c9b47a87eaa2a
jesus.barrio@gmail.com <jesus_barrio@hotmail.com>
2026-07-01 12:17:29 +0200

    d

--- landerist_tests/TestRegions/ScrapperTests.cs
+++ landerist_tests/TestRegions/ScrapperTests.cs
@@ -19,7 +19,7 @@ namespace landerist_tests
             //new Scraper().Start();
             //new Scraper().Scrape(page, false);
 
-            //new landerist_library.Scrape.Scraper().Scrape("https://www.remax.es/buscador-de-inmuebles/venta/suelo/madrid/navacerrada/todos/LODON-00801/", false);
+            //new landerist_library.Scrape.Scraper().Scrape("https://www.remax.es/buscador-de-inmuebles/venta/piso/alicante-alacant/cumbre-del-sol/todos/true009055/", false);
             //new landerist_library.Scrape.Scraper().TryApplyPreClassificationBeforeDownload(new landerist_library.Pages.Page("https://www.iadespana.es/anuncios/aspe-03680/venta/casa"));
             //new Scraper().TestSinglePage();
             //landerist_library.Scrape.PageSelector.SelectTop1();
